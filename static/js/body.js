4d68e251a0a3278c1110f631f817dc3ebe5f1fe3
David Sebastian Rodriguez Jimenez <88215514+David-Sebastian-Rodriguez@users.noreply.github.com>
2022-04-08 12:05:32 -0500

    Update body.js

--- static/js/body.js
+++ static/js/body.js
@@ -60,7 +60,7 @@ document.write(`
     </div>
 
 
-    <!--JavaScript at end of body for optimized loading-->
+    <!--JavaScript at end of body for optimized loading -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script
     type="text/javascript"
